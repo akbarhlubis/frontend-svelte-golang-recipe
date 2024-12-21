22595ba300e8465b6cf0a2e93020e7baaa4cf014
akbarhlubis <akbar_hamonangan_lubis@teknokrat.ac.id>
2024-12-21 18:15:09 +0700

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+
+  const wow = 12+2;
+</script>
+
+<main>
+<h1>Persatuan Bangsa</h1>
+</main>
+
+<style>
+  *{
+    user-select: none;
+  }
+</style>
